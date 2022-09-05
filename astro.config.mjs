c0979d128d565e813e07826cae0d6b7ee6d33410
JRedeker <jon.redeker@gmail.com>
2022-09-05 17:26:55 -0400

    add partytown for prioritization of site code

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
-
 import prefetch from "@astrojs/prefetch";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), mdx(), prefetch()]
+  integrations: [tailwind(), react(), mdx(), prefetch(), partytown()]
 });

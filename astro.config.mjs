c712234d309b1b7a809a873d57a9b0248a211599
JRedeker <jon.redeker@gmail.com>
2022-09-05 17:25:14 -0400

    add prefetch

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import mdx from "@astrojs/mdx";
+
+import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), mdx()]
+  integrations: [tailwind(), react(), mdx(), prefetch()]
 });

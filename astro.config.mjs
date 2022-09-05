c39f7706e7952780cb5ff0d59004549e6fbc0f2f
JRedeker <jon.redeker@gmail.com>
2022-09-05 17:33:32 -0400

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,11 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
 import prefetch from "@astrojs/prefetch";
-
 import partytown from "@astrojs/partytown";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), mdx(), prefetch(), partytown()]
+  integrations: [tailwind(), react(), mdx(), prefetch(), partytown(), sitemap()]
 });
